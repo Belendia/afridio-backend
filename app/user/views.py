82e42ed8f0973a5c8058a613280692ee5e7a5735
Belendia Serda <belendia@gmail.com>
2021-01-22 15:03:36 +0300

    Add about user endpoint

--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,6 @@
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 
 from user.serializers import AuthTokenSerializer, UserSerializer
 
@@ -16,3 +16,16 @@ class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
 
     serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return authentication user"""
+
+        return self.request.user
