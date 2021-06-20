e1e569a669989e36d2b391769374c266599a6b3f
Belendia Serda <belendia@gmail.com>
2021-06-20 12:36:10 +0300

    Refactor resize image task

--- app/apps/common/utils/file_processor.py
+++ app/apps/common/utils/file_processor.py
@@ -36,3 +36,21 @@ def insert_text_in_file_name(filename, text):
 def replace_ext(filename, ext):
     name = os.path.splitext(filename)[0]
     return "{}.{}".format(name, ext)
+
+
+def get_base_size(uploaded_image, width):
+    actual_img_width, actual_img_height = uploaded_image.width, uploaded_image.height
+    ratio = width / actual_img_width
+    return int(actual_img_width * ratio), int(actual_img_height * ratio)
+
+
+def get_watermark_size(uploaded_image, logo_max_width_height_ratio):
+    max_width_height = max(uploaded_image.width, uploaded_image.height)
+    return (int(max_width_height * logo_max_width_height_ratio),
+            int(max_width_height * logo_max_width_height_ratio))
+
+
+def get_watermark_top_left(water_mark_image, logo_top_left_ratio):
+    actual_img_width, actual_img_height = water_mark_image.width, water_mark_image.height
+    return (int(actual_img_width * logo_top_left_ratio),
+            int(actual_img_height * logo_top_left_ratio))
--- app/apps/media/tasks/resize_image_task.py
+++ app/apps/media/tasks/resize_image_task.py
@@ -7,21 +7,13 @@ from tempfile import TemporaryFile
 from django.core.files.storage import default_storage
 from django.conf import settings
 
-# from apps.media.models import Media
-from apps.common.utils.file_processor import preserve_original_file
+from apps.common.utils.file_processor import preserve_original_file, get_base_size, get_watermark_size, \
+    get_watermark_top_left
 
 
 @task(name='resize_image')
 def resize_image(filename, watermark, logo_max_width_height_ratio, logo_top_left_ratio, width):
-    # if settings.COVER_IMAGE_CROP_SIZES.__len__() > 0:
-    #     original_filename, error = preserve_original_file(instance.filename)
-    #
-    #     if error:
-    #         return
-
     try:
-        # for i, resize_shape in enumerate(settings.COVER_IMAGE_CROP_SIZES):
-
         if filename:
             client = default_storage.client
             img_file = client.get_object(settings.
@@ -45,22 +37,11 @@ def resize_image(filename, watermark, logo_max_width_height_ratio, logo_top_left
 
             transparent = transparent.resize(get_base_size(base_img, width))
 
-            # img_name = os.path.splitext(filename)[0]
-            # resize_img_name = "{}{}-w{}.{}".format(slug, id, width, 'png')
-
             f = TemporaryFile()
             transparent.save(f, 'PNG')
             f.seek(0)
             length = len(f.read())
             f.seek(0)
-
-            # resize_img_name = "{}.{}".format(img_name, 'png')
-            # try:
-            #     uploaded_image.path.name = resize_img_name
-            #     uploaded_image.save()
-            # except Media.DoesNotExist:
-            #     logging.error("Image with slug: {} Not found. Unable to "
-            #                   "update file in image".format(slug))
 
             # save the modified object
             client.put_object(settings.MINIO_STORAGE_MEDIA_BUCKET_NAME,
@@ -74,19 +55,4 @@ def resize_image(filename, watermark, logo_max_width_height_ratio, logo_top_left
         logging.error(ex)
 
 
-def get_base_size(uploaded_image, width):
-    actual_img_width, actual_img_height = uploaded_image.width, uploaded_image.height
-    ratio = width / actual_img_width
-    return int(actual_img_width * ratio), int(actual_img_height * ratio)
 
-
-def get_watermark_size(uploaded_image, logo_max_width_height_ratio):
-    max_width_height = max(uploaded_image.width, uploaded_image.height)
-    return (int(max_width_height * logo_max_width_height_ratio),
-            int(max_width_height * logo_max_width_height_ratio))
-
-
-def get_watermark_top_left(water_mark_image, logo_top_left_ratio):
-    actual_img_width, actual_img_height = water_mark_image.width, water_mark_image.height
-    return (int(actual_img_width * logo_top_left_ratio),
-            int(actual_img_height * logo_top_left_ratio))
