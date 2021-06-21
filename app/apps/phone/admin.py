edec66a40d3095f2c66aebe9185647e0c7e1bd0c
Belendia Serda <belendia@gmail.com>
2021-06-21 16:38:34 +0300

    Improve the admin interface for phone verification model

--- app/apps/phone/admin.py
+++ app/apps/phone/admin.py
@@ -3,7 +3,9 @@ from . import models
 
 
 class PhoneNumberAdmin(admin.ModelAdmin):
-    list_display = ['phone_number', 'is_verified']
+    list_display = ('phone_number', 'security_code', 'is_verified')
+    search_fields = ("phone_number",)
+    list_filter = ("is_verified",)
 
 
 admin.site.register(models.PhoneVerification, PhoneNumberAdmin)
